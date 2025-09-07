1944599f05386502f2a832fa358872ea4fe344e2
James Benner <hello@jamesbenner.com>
2025-09-07 17:01:17 -0400

    Cruft cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {
   type ChangeEvent,
   type MouseEventHandler,
-  type ReactNode,
   useContext,
   useState
 } from 'react'
@@ -26,7 +25,6 @@ function CurrentVersionPrompt({ handleOnChange: passedHandleOnChange, handleOnCl
   const validateCurrentVersion = (event: ChangeEvent<HTMLInputElement>): void => {
     const version = event.target.value;
 
-    // setIsCurrentVersionValid(semver.valid(version) !== null);
     setIsCurrentVersionValid(is(version).valid());
   }
   const handleOnChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -101,33 +99,7 @@ function App() {
         </IncrementTypeContext>
       </CurrentVersionContext>
     </>
-  )
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.tsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
+  );
 }
 
-export default App
+export default App;
